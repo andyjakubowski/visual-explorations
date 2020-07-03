f7cc76981ff125162eac9a6fdfdea890e39f7012
Andy Jakubowski <hello@andyjakubowski.com>
2020-07-03 17:48:14 +0200

    Add playsinline attribute to video in example 13

--- 13/main.js
+++ 13/main.js
@@ -7,6 +7,7 @@ function injectVideo() {
   const video = document.createElement('video');
   video.setAttribute('autoplay', '');
   video.setAttribute('loop', '');
+  video.setAttribute('playsinline', 'true');
   video.setAttribute('muted', '');
   video.setAttribute('src', '../videos/pride.mp4');
   main.append(video);
