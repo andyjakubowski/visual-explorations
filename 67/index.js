7b6091ccfa5db8c9631b2e92c3174280c4cfca61
Andy Jakubowski <hello@andyjakubowski.com>
2021-02-02 09:15:25 +0100

    Update 67 - Stopwatch

--- 67/index.js
+++ 67/index.js
@@ -2,20 +2,31 @@ function handleDOMContentLoaded() {
   const INTERVAL_MS = 10;
   const elLabel = document.getElementById('msLabel');
   const elButton = document.getElementById('button');
+  const elDateStart = document.getElementById('dateStart');
+  const elDateEnd = document.getElementById('dateEnd');
   let ms = 0;
   let isRunning = false;
+  let dateStart = null;
+  let dateEnd = null;
 
   const clickHandler = (function makeClickHandler() {
     let id;
 
     return function clickHandler() {
       if (!isRunning) {
+        dateStart = !dateStart ? Date.now() : null;
+        dateEnd = null;
+        elDateEnd.textContent = '';
         id = setInterval(() => {
-          ms += INTERVAL_MS;
+          elDateStart.textContent = String(dateStart);
+          ms = Date.now() - dateStart;
+          dateEnd = Date.now();
           elLabel.textContent = ms;
         }, INTERVAL_MS);
       } else {
         clearInterval(id);
+        dateStart = null;
+        elDateEnd.textContent = String(dateEnd);
       }
       isRunning = !isRunning;
     };
